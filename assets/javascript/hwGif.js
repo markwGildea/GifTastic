e456ab11f482abae0dc95b2ef341f62f0e43b16b
Mark Gildea <gildeamark1@gmail.com>
2016-11-09 16:19:33 -0600

    adding

--- assets/javascript/hwGif.js
+++ assets/javascript/hwGif.js
@@ -1,1 +1,22 @@
-$(document).ready
+var gifs = [];
+
+function displayGifs() {
+
+    var giphy = $(this).data('data-name');
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + giphy + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function(response) {
+
+        var giphyDiv = $('<div class="giphy">');
+
+        var rating = response.Rated;
+
+        var listRating = $("<p>").text( "Rating" + rating);
+
+        giphyDiv.append(listRating);
+
+    });
+}
